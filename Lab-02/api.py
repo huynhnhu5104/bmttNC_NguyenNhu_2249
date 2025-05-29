19836994732ecc19b4b9a82fbbfa6a88fd806e2f
nhu <nguyenhuynhnhu4069@gmail.com>
2025-05-29 13:06:51 +0700

    Update cipher implementations and API

--- Lab-02/api.py
+++ Lab-02/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify # type: ignore
 from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
 from cipher.railfence import RailFenceCipher
